02797a65a94da7043d9b6c6f52d3deb410f51e74
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-06-28 01:03:13 +0530

    ADD: create store, dispatch actions, subscribe to changes

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+// Action Types //
 const BUY_CAKE = 'BUY_CAKE';
 
+// Action Creators //
 function buyCake() {
     return { type: BUY_CAKE };
 }
 
+// Initial State //
 const initialState = {
     numOfCakes: 10
 };
 
+// Reducer //
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case BUY_CAKE: return { ...state, numOfCakes: state.numOfCakes - 1 };
         default: return state;
     }
 };
+
+// Store //
+const store = createStore(reducer);
+
+// getState() //
+console.log("Initial State:", store.getState());
+
+// Subscribe //
+const unsubscribe = store.subscribe(() => console.log("Updated State:", store.getState()));
+
+// Dispatch //
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+// Unsubscibe //
+unsubscribe();
