a1a747472861db6f1fb03a987e0a259e8851f861
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-06-30 00:55:56 +0530

    ADD: actions to stockup cake and icecream

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ const { createLogger } = reduxLogger;
 
 // Action Types //
 const BUY_CAKE = 'BUY_CAKE';
+const STOCKUP_CAKE = 'STOCKUP_CAKE';
 const BUY_ICECREAM = 'BUY_ICECREAM';
+const STOCKUP_ICECREAM = 'STOCKUP_ICECREAM';
 
 // Action Creators //
 function buyCake() {
     return { type: BUY_CAKE };
 }
 
+function stockupCake(numOfCakes) {
+    return { type: STOCKUP_CAKE, numOfCakes };
+}
+
 function buyIceCream() {
     return { type: BUY_ICECREAM };
+}
+
+function stockupIceCream(numOfIceCreams) {
+    return { type: STOCKUP_ICECREAM, numOfIceCreams };
 }
 
 // Initial States //
@@ -29,6 +39,7 @@ const initialIceCreamState = {
 const cakeReducer = (state = initialCakeState, action) => {
     switch (action.type) {
         case BUY_CAKE: return { ...state, numOfCakes: state.numOfCakes - 1 };
+        case STOCKUP_CAKE: return { ...state, numOfCakes: state.numOfCakes + action.numOfCakes };
         default: return state;
     }
 };
@@ -36,6 +47,7 @@ const cakeReducer = (state = initialCakeState, action) => {
 const iceCreamReducer = (state = initialIceCreamState, action) => {
     switch (action.type) {
         case BUY_ICECREAM: return { ...state, numOfIceCreams: state.numOfIceCreams - 1 };
+        case STOCKUP_ICECREAM: return { ...state, numOfIceCreams: state.numOfIceCreams + action.numOfIceCreams };
         default: return state;
     }
 };
@@ -64,8 +76,11 @@ store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyIceCream());
 store.dispatch(buyIceCream());
+store.dispatch(stockupCake(10));
+store.dispatch(buyIceCream());
+store.dispatch(stockupIceCream(15));
 
 // Unsubscibe //
 unsubscribe();
 
-// 10
+// 12
