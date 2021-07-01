f34e057f0f39884c716add1c0a840cd8c5a3e789
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-07-02 01:30:08 +0530

    ADD: action creators, reducer, store for async actions

---
+++ asyncActions.js
@@ -0,0 +1,34 @@
+const redux = require("redux");
+const { createStore } = redux;
+
+// Action Types //
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
+
+// Action Creators //
+const fetchUserRequest = () => ({ type: FETCH_USERS_REQUEST });
+const fetchUserSuccess = users => ({ type: FETCH_USERS_SUCCESS, payload: users });
+const fetchUserFailure = error => ({ type: FETCH_USERS_FAILURE, payload: error });
+
+// Initial State //
+
+const initialState = {
+    loading: false,
+    users: [],
+    error: ''
+}
+
+// Reducer //
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_USERS_REQUEST: return { ...state, loading: true };
+        case FETCH_USERS_SUCCESS: return { ...state, loading: false, users: action.payload, error: '' };
+        case FETCH_USERS_FAILURE: return { ...state, loading: false, users: [], error: action.payload };
+    }
+};
+
+// Store //
+const store = createStore(reducer);
+
+
